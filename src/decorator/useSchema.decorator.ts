8645135e04b40b0fc1507a25783d3805ac29f802
jerry <jerry@freshworks.io>
2021-03-11 10:43:25 -0800

    upgrade yup and other packages to latest version

--- src/decorator/useSchema.decorator.ts
+++ src/decorator/useSchema.decorator.ts
@@ -1,6 +1,6 @@
-import { Schema } from 'yup';
+import { SchemaOf } from 'yup';
 
-export function UseSchema(schema: Schema<{}>) {
+export function UseSchema(schema: SchemaOf<{}>) {
   return function (constructor: Function) {
     constructor.prototype.schema = schema;
   };
